3a6e5565368ca2723d35cc5c9af7e499e6632acd
Zachary Childers <zdchilders10@sccsc.edu>
2021-01-24 21:59:34 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace ZacharyChilders_CPT_185_Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picFace1 = new System.Windows.Forms.PictureBox();
+            this.picFace2 = new System.Windows.Forms.PictureBox();
+            this.picFace3 = new System.Windows.Forms.PictureBox();
+            this.picFace5 = new System.Windows.Forms.PictureBox();
+            this.picFace4 = new System.Windows.Forms.PictureBox();
+            this.picBack4 = new System.Windows.Forms.PictureBox();
+            this.picBack1 = new System.Windows.Forms.PictureBox();
+            this.picBack2 = new System.Windows.Forms.PictureBox();
+            this.picBack3 = new System.Windows.Forms.PictureBox();
+            this.picBack5 = new System.Windows.Forms.PictureBox();
+            this.btnFn = new System.Windows.Forms.Button();
+            this.clr = new System.Windows.Forms.Button();
+            this.ext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.abt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFace1
+            // 
+            this.picFace1.Image = ((System.Drawing.Image)(resources.GetObject("picFace1.Image")));
+            this.picFace1.Location = new System.Drawing.Point(89, 106);
+            this.picFace1.Name = "picFace1";
+            this.picFace1.Size = new System.Drawing.Size(51, 71);
+            this.picFace1.TabIndex = 0;
+            this.picFace1.TabStop = false;
+            this.picFace1.Click += new System.EventHandler(this.picFace1_Click);
+            // 
+            // picFace2
+            // 
+            this.picFace2.Image = ((System.Drawing.Image)(resources.GetObject("picFace2.Image")));
+            this.picFace2.Location = new System.Drawing.Point(146, 106);
+            this.picFace2.Name = "picFace2";
+            this.picFace2.Size = new System.Drawing.Size(51, 71);
+            this.picFace2.TabIndex = 1;
+            this.picFace2.TabStop = false;
+            this.picFace2.Click += new System.EventHandler(this.picFace2_Click);
+            // 
+            // picFace3
+            // 
+            this.picFace3.Image = ((System.Drawing.Image)(resources.GetObject("picFace3.Image")));
+            this.picFace3.Location = new System.Drawing.Point(203, 106);
+            this.picFace3.Name = "picFace3";
+            this.picFace3.Size = new System.Drawing.Size(51, 71);
+            this.picFace3.TabIndex = 2;
+            this.picFace3.TabStop = false;
+            this.picFace3.Click += new System.EventHandler(this.picFace3_Click);
+            // 
+            // picFace5
+            // 
+            this.picFace5.Image = ((System.Drawing.Image)(resources.GetObject("picFace5.Image")));
+            this.picFace5.Location = new System.Drawing.Point(317, 106);
+            this.picFace5.Name = "picFace5";
+            this.picFace5.Size = new System.Drawing.Size(51, 71);
+            this.picFace5.TabIndex = 3;
+            this.picFace5.TabStop = false;
+            this.picFace5.Click += new System.EventHandler(this.picFace5_Click);
+            // 
+            // picFace4
+            // 
+            this.picFace4.Image = ((System.Drawing.Image)(resources.GetObject("picFace4.Image")));
+            this.picFace4.Location = new System.Drawing.Point(260, 106);
+            this.picFace4.Name = "picFace4";
+            this.picFace4.Size = new System.Drawing.Size(51, 71);
+            this.picFace4.TabIndex = 4;
+            this.picFace4.TabStop = false;
+            this.picFace4.Click += new System.EventHandler(this.picFace4_Click);
+            // 
+            // picBack4
+            // 
+            this.picBack4.Image = ((System.Drawing.Image)(resources.GetObject("picBack4.Image")));
+            this.picBack4.Location = new System.Drawing.Point(260, 106);
+            this.picBack4.Name = "picBack4";
+            this.picBack4.Size = new System.Drawing.Size(51, 71);
+            this.picBack4.TabIndex = 5;
+            this.picBack4.TabStop = false;
+            this.picBack4.Click += new System.EventHandler(this.picBack4_Click);
+            // 
+            // picBack1
+            // 
+            this.picBack1.Image = ((System.Drawing.Image)(resources.GetObject("picBack1.Image")));
+            this.picBack1.Location = new System.Drawing.Point(89, 106);
+            this.picBack1.Name = "picBack1";
+            this.picBack1.Size = new System.Drawing.Size(51, 71);
+            this.picBack1.TabIndex = 6;
+            this.picBack1.TabStop = false;
+            this.picBack1.Click += new System.EventHandler(this.picBack1_Click);
+            // 
+            // picBack2
+            // 
+            this.picBack2.Image = ((System.Drawing.Image)(resources.GetObject("picBack2.Image")));
+            this.picBack2.Location = new System.Drawing.Point(146, 106);
+            this.picBack2.Name = "picBack2";
+            this.picBack2.Size = new System.Drawing.Size(51, 71);
+            this.picBack2.TabIndex = 7;
+            this.picBack2.TabStop = false;
+            this.picBack2.Click += new System.EventHandler(this.picBack2_Click);
+            // 
+            // picBack3
+            // 
+            this.picBack3.Image = ((System.Drawing.Image)(resources.GetObject("picBack3.Image")));
+            this.picBack3.Location = new System.Drawing.Point(203, 106);
+            this.picBack3.Name = "picBack3";
+            this.picBack3.Size = new System.Drawing.Size(51, 71);
+            this.picBack3.TabIndex = 8;
+            this.picBack3.TabStop = false;
+            this.picBack3.Click += new System.EventHandler(this.picBack3_Click);
+            // 
+            // picBack5
+            // 
+            this.picBack5.Image = ((System.Drawing.Image)(resources.GetObject("picBack5.Image")));
+            this.picBack5.Location = new System.Drawing.Point(317, 106);
+            this.picBack5.Name = "picBack5";
+            this.picBack5.Size = new System.Drawing.Size(51, 71);
+            this.picBack5.TabIndex = 9;
+            this.picBack5.TabStop = false;
+            this.picBack5.Click += new System.EventHandler(this.picBack5_Click);
+            // 
+            // btnFn
+            // 
+            this.btnFn.BackColor = System.Drawing.Color.DimGray;
+            this.btnFn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFn.ForeColor = System.Drawing.Color.White;
+            this.btnFn.Location = new System.Drawing.Point(73, 190);
+            this.btnFn.Name = "btnFn";
+            this.btnFn.Size = new System.Drawing.Size(307, 34);
+            this.btnFn.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.btnFn, "Card name output location");
+            this.btnFn.UseVisualStyleBackColor = false;
+            // 
+            // clr
+            // 
+            this.clr.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.clr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clr.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clr.ForeColor = System.Drawing.Color.Black;
+            this.clr.Location = new System.Drawing.Point(12, 230);
+            this.clr.Name = "clr";
+            this.clr.Size = new System.Drawing.Size(76, 29);
+            this.clr.TabIndex = 11;
+            this.clr.Text = "&Clear";
+            this.toolTip1.SetToolTip(this.clr, "Clears the Deck");
+            this.clr.UseVisualStyleBackColor = false;
+            this.clr.Click += new System.EventHandler(this.clr_Click);
+            // 
+            // ext
+            // 
+            this.ext.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ext.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ext.ForeColor = System.Drawing.Color.Black;
+            this.ext.Location = new System.Drawing.Point(362, 230);
+            this.ext.Name = "ext";
+            this.ext.Size = new System.Drawing.Size(76, 29);
+            this.ext.TabIndex = 12;
+            this.ext.Text = "&Exit";
+            this.toolTip1.SetToolTip(this.ext, "Exits the Game");
+            this.ext.UseVisualStyleBackColor = false;
+            this.ext.Click += new System.EventHandler(this.ext_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(130, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Click a Card to Reveal the Face!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 52);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox1, "C#!!!");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // abt
+            // 
+            this.abt.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.abt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.abt.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.abt.ForeColor = System.Drawing.Color.Black;
+            this.abt.Location = new System.Drawing.Point(164, 230);
+            this.abt.Name = "abt";
+            this.abt.Size = new System.Drawing.Size(122, 29);
+            this.abt.TabIndex = 15;
+            this.abt.Text = "&About Project";
+            this.toolTip1.SetToolTip(this.abt, "Read More");
+            this.abt.UseVisualStyleBackColor = false;
+            this.abt.Click += new System.EventHandler(this.abt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(143, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Also Try Double-Clicking!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(451, 277);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.abt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ext);
+            this.Controls.Add(this.clr);
+            this.Controls.Add(this.btnFn);
+            this.Controls.Add(this.picBack5);
+            this.Controls.Add(this.picBack3);
+            this.Controls.Add(this.picBack2);
+            this.Controls.Add(this.picBack1);
+            this.Controls.Add(this.picBack4);
+            this.Controls.Add(this.picFace4);
+            this.Controls.Add(this.picFace5);
+            this.Controls.Add(this.picFace3);
+            this.Controls.Add(this.picFace2);
+            this.Controls.Add(this.picFace1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Card Revealer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFace1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFace4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFace1;
+        private System.Windows.Forms.PictureBox picFace2;
+        private System.Windows.Forms.PictureBox picFace3;
+        private System.Windows.Forms.PictureBox picFace5;
+        private System.Windows.Forms.PictureBox picFace4;
+        private System.Windows.Forms.PictureBox picBack4;
+        private System.Windows.Forms.PictureBox picBack1;
+        private System.Windows.Forms.PictureBox picBack2;
+        private System.Windows.Forms.PictureBox picBack3;
+        private System.Windows.Forms.PictureBox picBack5;
+        private System.Windows.Forms.Button btnFn;
+        private System.Windows.Forms.Button clr;
+        private System.Windows.Forms.Button ext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button abt;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿//Zachary Childers 
+//CPT-185-A01S
+//Lab-2
+
+using System;
+using System.Windows.Forms;
+
+namespace ZacharyChilders_CPT_185_Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picBack1_Click(object sender, EventArgs e)
+        {
+            picFace1.Visible = true;
+            picBack1.Visible = false;
+            btnFn.Text = "Your card is the 8 of Diamonds";
+        }
+
+        private void ext_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks for playing, come back any time!");
+            Close();
+        }
+
+        private void picBack2_Click(object sender, EventArgs e)
+        {
+            picFace2.Visible = true;
+            picBack2.Visible = false;
+            btnFn.Text = "Your card is the 2 of Clubs";
+        }
+
+        private void picBack3_Click(object sender, EventArgs e)
+        {
+            picFace3.Visible = true;
+            picBack3.Visible = false;
+            btnFn.Text = "Your card is the King of Clubs";
+        }
+
+        private void picBack4_Click(object sender, EventArgs e)
+        {
+            picFace4.Visible = true;
+            picBack4.Visible = false;
+            btnFn.Text = "Your card is the Ace of Spades";
+        }
+
+        private void picBack5_Click(object sender, EventArgs e)
+        {
+            picFace5.Visible = true;
+            picBack5.Visible = false;
+            btnFn.Text = "Your card is the Joker";
+        }
+
+        private void clr_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack1.Visible = true;
+            picBack2.Visible = true;
+            picBack3.Visible = true;
+            picBack4.Visible = true;
+            picBack5.Visible = true;
+
+
+        }
+
+        private void picFace1_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack1.Visible = true;
+            picFace1.Visible = false;
+            //flip over card
+
+        }
+
+        private void picFace2_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack2.Visible = true;
+            picFace2.Visible = false;
+            //flip over card
+        }
+
+        private void picFace3_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack3.Visible = true;
+            picFace3.Visible = false;
+            //flip over card
+        }
+
+        private void picFace4_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack4.Visible = true;
+            picFace4.Visible = false;
+            //flip over card
+        }
+
+        private void picFace5_Click(object sender, EventArgs e)
+        {
+            btnFn.Text = " ";
+            picBack5.Visible = true;
+            picFace5.Visible = false;
+            //flip over card
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void abt_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a C# Learning Project, as part of Spartanburg Community College's Programming Course. Thank you for being part of my learning process!");
+        }
+
+       
+    }
+}
